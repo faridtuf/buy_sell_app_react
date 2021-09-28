35e103c2f9ddd73f548773f94574b7e320cbc92c
faridtuf <ghulam.farid@tuf.edu.pk>
2021-09-28 13:00:21 +0500

    Update product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,6 +6,7 @@ import network3 from "../images/network3.png"
 import network4 from "../images/network4.png"
 import network5 from "../images/network5.png"
 import network6 from "../images/network6.png"
+import $ from "jquery";
 export default class Product extends Component{
     render(){
         return(
@@ -23,35 +24,106 @@ export default class Product extends Component{
                             <div className="row align-items-center">
                                 <div className="col-2"><h5 className="network">NETWORK</h5></div>
                                 <div className="col">
-                                    <input type="radio" name="network"/>
-                                    <img src={network1} alt="" className="network-image"/>
+                                   <div className="bg-light position-relative">
+                                       <input type="radio" name="network" value="1"/>
+                                       <img src={network1} alt="" className="network-image"/>
+                                   </div>
                                 </div>
                                 <div className="col">
-                                    <input type="radio" name="network"/>
-                                    <img src={network2} alt="" className="network-image"/>
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="2"/>
+                                        <img src={network2} alt="" className="network-image"/>
+                                    </div>
                                 </div>
                                 <div className="col">
-                                    <input type="radio" name="network"/>
-                                    <img src={network3} alt="" className="network-image"/>
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="3"/>
+                                        <img src={network3} alt="" className="network-image"/>
+                                    </div>
                                 </div>
                                 <div className="col">
-                                    <input type="radio" name="network"/>
-                                    <img src={network4} alt="" className="network-image"/>
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="4"/>
+                                        <img src={network4} alt="" className="network-image"/>
+                                    </div>
                                 </div>
                                 <div className="col">
-                                    <input type="radio" name="network"/>
-                                    <img src={network5} alt="" className="network-image"/>
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="5"/>
+                                        <img src={network5} alt="" className="network-image"/>
+                                    </div>
                                 </div>
                                 <div className="col">
-                                    <input type="radio" name="network"/>
-                                    <img src={network6} alt="" className="network-image"/>
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="6"/>
+                                        <img src={network6} alt="" className="network-image"/>
+                                    </div>
                                 </div>
+                            </div>
+                            <div className="row align-items-center position-relative">
+                                <div className="col-2"><h5 className="network">CAPACITY</h5></div>
+                                <div className="col">
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="1"/>
+                                        <p>128GB</p>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="2"/>
+                                        <p>256GB</p>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="3"/>
+                                        <p>512GB</p>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="4"/>
+                                        <p>1TB</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center position-relative">
+                                <div className="col-2"><h5 className="network">CONDITION</h5></div>
+                                <div className="col">
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="1"/>
+                                        <p>Like New</p>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="2"/>
+                                        <p>Good</p>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="3"/>
+                                        <p>Poor</p>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="bg-light position-relative">
+                                        <input type="radio" name="network" value="4"/>
+                                        <p>Faulty</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="filters1" className="desc">
+                                2 Cars Selected
+                            </div>
+                            <div id="filters2" className="desc">
+                                3 Cars
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         )
-
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import $ from 'jquery'
 
 ReactDOM.render(
     <Router>
@@ -11,7 +12,14 @@ ReactDOM.render(
     </Router>,
   document.getElementById('root')
 );
+$(document).ready(function() {
+    $("input[name$='network']").click(function() {
+        var test = $(this).val();
 
+        $("div.desc").hide();
+        $("#filters" + test).show();
+    });
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
