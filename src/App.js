4e7d4677a87507f1044d545234925ea151dcd0db
faridtuf <ghulam.farid@tuf.edu.pk>
2021-10-28 11:29:48 +0500

    Navbar switched

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import Navbar from './components/Navbar';
+import BuyNavbar from './components/BuyNavbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
@@ -20,26 +21,39 @@ import SignUp from './components/signup';
 import QuickBrandProduct from './components/QuickBrandProduct';
 import BuyProduct from './components/BuyProduct';
 
+const MainNav = ({exact, path, component: Component}) => (
+    <Route exact={exact} path={path} render={(props) => (
+        <div>
+            <Navbar/>
+            <Component {...props}/>
+        </div>
+    )}/>
+)
+const BuyNav = ({exact, path, component: Component}) => (
+    <Route exact={exact} path={path} render={(props) => (
+        <div>
+            <BuyNavbar/>
+            <Component {...props}/>
+        </div>
+    )}/>
+)
 function App() {
     return (
         <React.Fragment>
-            <Navbar/>
             <Switch>
-                
-                <Route exact path="/details" component={Details}/>
-                <Route exact path="/cart" component={Cart}/>
-                
-                <Route path="/sell" component={Sell}/>
-                <Route exact path="/brand" component={brand}/>
-                <Route  path="/product" component={Product}/>
-                <Route  path="/buy/product" component={BuyProduct}/>
-                <Route  path="/brands/product/" component={BrandProduct}/>
-                <Route  path="/quick/brand/product/" component={QuickBrandProduct}/>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/signup" component={SignUp}/>
-                <Route exact path="/" component={ProductList}/>
-                <Route exact path="/buy" component={Buy}/>
-                <Route  component={Default}/>
+                <MainNav exact path="/details" component={Details}/>
+                <MainNav exact path="/cart" component={Cart}/>
+                <MainNav path="/sell" component={Sell}/>
+                <MainNav exact path="/brand" component={brand}/>
+                <MainNav  path="/product" component={Product}/>
+                <MainNav  path="/buy/product" component={BuyProduct}/>
+                <MainNav  path="/brands/product/" component={BrandProduct}/>
+                <MainNav  path="/quick/brand/product/" component={QuickBrandProduct}/>
+                <MainNav exact path="/login" component={Login}/>
+                <MainNav exact path="/signup" component={SignUp}/>
+                <MainNav exact path="/" component={ProductList}/>
+                <BuyNav exact path="/buy" component={Buy}/>
+                <MainNav  component={Default}/>
 
             </Switch>
             <Footer/>
