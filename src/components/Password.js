a6e1aa46cc5f2c55a5656fd5be6a724f190b2906
ahmad225 <ahmadshahid5776@gmail.com>
2021-11-10 14:04:47 +0500

    Error Removed

--- src/components/Password.js
+++ src/components/Password.js
@@ -1,49 +1,69 @@
 import React, { Component } from "react";
 import axios from "axios";
-import '../App.css';
-
+import "../App.css";
 
 export default class Password extends Component {
-    render() {
-        return (
-
-            <div class="container-fluid">
-                <div class="vertical-menu">
-                   
-                <a href="/my/account" className="active">Account</a>
-                                <a href="/my/order"><i className="glyphicon glyphicon-gift" />My Order</a><i />
-                                <a href="/my/amount"><i className="glyphicon glyphicon-user">Edit Account</i></a>
-                                <a href="/sell/password"><i className="glyphicon glyphicon-lock">Password</i></a>
-                                <a href="/sell/address"><i className="glyphicon glyphicon-book"></i>Address Books</a>
-                                <a href="/sell/acclogout"><i className="glyphicon glyphicon-off"></i>Logout</a>
-
-                </div>
-                <div class="container">
-                    <div class="card">
-                        <div class="row">
-                            <div class="mb-2 row">
-                                <label for="inputPassword" class="col-sm-3 col-form-label">*Password</label>
-                                <div class="col-4">
-                                    <input type="password" class="form-control" id="inputPassword" />
-                                </div>
-                            </div>
-                            <div class="mb-2 row">
-                                <label for="inputPassword" class="col-sm-3 col-form-label">*Password Confirm</label>
-                                <div class="col-4">
-                                    <input type="password" class="form-control" id="inputPassword" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="password-m">
-                        <button type="submit" className="button-form">Continue</button>
-                        </div> 
-                    </div>
-
-
-                </div>
-            </div>
-        )
-
-
-    }
+  render() {
+    return (
+      <div class="container">
+          <br/>
+          <br/>
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-4">
+            <form class="form" action="/action_page.php">
+              <label for="inputPassword" class="mb-2 mr-sm-2">
+                Password:
+              </label>
+              <input
+                type="password"
+                class="form-control mb-2 mr-sm-2"
+                id="inputPassword"
+                placeholder="Enter Password"
+                name="Password"
+              />
+              <label for="pwd2" class="mb-2 mr-sm-2">
+                Reset Password:
+              </label>
+              <input
+                type="password"
+                class="form-control mb-2 mr-sm-2"
+                id="pwd2"
+                placeholder="New password"
+                name="pswd"
+              />
+              <br/>
+              <div className="center">
+              <button type="submit" class="button-form font center">
+                Submit
+              </button>
+              </div>
+            </form>
+          </div>
+          <div class="col-md-2"></div>
+          <div class="vertical-menu col-md-3">
+          <a href="/my/account" className="active">
+            Account
+          </a>
+          <a href="/my/order">
+            <i className="glyphicon glyphicon-gift" />My Order</a>
+          <i />
+          <a href="/my/amount">
+            <i className="glyphicon glyphicon-user">Edit Account</i>
+          </a>
+          <a href="/sell/password">
+            <i className="glyphicon glyphicon-lock">Password</i>
+          </a>
+          <a href="/sell/address">
+            <i className="glyphicon glyphicon-book"></i>Address Books
+          </a>
+          <a href="/sell/acclogout">
+            <i class="glyphicon glyphicon-off"></i>Logout
+          </a>
+        </div>
+        </div>
+        
+      </div>
+    );
+  }
 }
